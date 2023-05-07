08999011c3ac1208ae4f70ba3d415846a1f987f3
DotWith <dotwith@tutanota.com>
2023-05-07 17:33:39 -0400

    Add copy trait to color correctly

--- src/css.rs
+++ src/css.rs
@@ -48,15 +48,13 @@ pub enum Unit {
     Px,
 }
 
-#[derive(Debug, Clone, PartialEq, Default)]
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
 pub struct Color {
     pub r: u8,
     pub g: u8,
     pub b: u8,
     pub a: u8,
 }
-
-impl Copy for Color {}
 
 pub type Specificity = (usize, usize, usize);
 
